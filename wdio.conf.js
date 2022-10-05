bf1887c2583d2a011b4842e20222779cf21fd7da
oklinylkoorb <nylkoorbokli@gmail.com>
2022-10-05 15:36:55 +0300

    Test Update 2.0.2

--- wdio.conf.js
+++ wdio.conf.js
@@ -237,11 +237,12 @@ exports.config = {
      * @param {Boolean} result.passed    true if test has passed, otherwise false
      * @param {Object}  result.retries   informations to spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-    // },
-
-
-    /**
+     afterTest: async function(test, context, { error, result, duration, passed, retries }) {
+        if (!passed) {
+            await browser.takeScreenshot();
+        }
+    },
+       /**
      * Hook that gets executed after the suite has ended
      * @param {Object} suite suite details
      */
